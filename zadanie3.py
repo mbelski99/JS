5f63f534833bd3749f19db9a72fa03145fd41561
mbelski99 <mbelski99@gmail.com>
2021-01-11 23:29:37 +0100

    a

--- zadanie3.py
+++ zadanie3.py
@@ -3,9 +3,9 @@ x=0
 while x<5:
     word1=input('Wpisz słowo:')
     if word1!=word:
-        print ("Źle! Spróbuj jeszcze raz")
+        print ("Źle! Spróbuj jeszcze raz!")
     else:
-        print ("Gratulacje")
+        print ("Gratulacje :D")
         break
     x+=1
 if x==5:
