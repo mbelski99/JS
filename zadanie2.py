5646c240dcb771ca82b08d38815d3804c5541d7b
mbelski99 <mbelski99@gmail.com>
2021-01-11 23:31:44 +0100

    a

--- zadanie2.py
+++ zadanie2.py
@@ -1,21 +1,21 @@
 list_to_sort = [[3, 2, 3], [2, 0, 2], [3, 0, 1], [3, 1, 0]]
-i_outer=0
-i_inner=0
+i_o=0
+i_i=0
 tmp_i=0
 tmp=list_to_sort[0]
 
-while i_outer<4:
-    i_inner=i_outer+1
-    tmp_i=i_outer
-    while i_inner<4:
-        if list_to_sort[tmp_i][1]>=list_to_sort[i_inner][1]:
-            if list_to_sort[tmp_i][1]==list_to_sort[i_inner][1]:
-                if list_to_sort[tmp_i][2]>list_to_sort[i_inner][2]:
-                    tmp_i=i_inner
+while i_o<4:
+    i_inner=i_o+1
+    tmp_i=i_o
+    while i_i<4:
+        if list_to_sort[tmp_i][1]>=list_to_sort[i_i][1]:
+            if list_to_sort[tmp_i][1]==list_to_sort[i_i][1]:
+                if list_to_sort[tmp_i][2]>list_to_sort[i_i][2]:
+                    tmp_i=i_i
             else:
-                tmp_i=i_inner
-        i_inner+=1
+                tmp_i=i_i
+        i_i+=1
     tmp=list_to_sort[tmp_i]
-    list_to_sort[tmp_i]=list_to_sort[i_outer]
-    list_to_sort[i_outer]=tmp
-    i_outer+=1
+    list_to_sort[tmp_i]=list_to_sort[i_o]
+    list_to_sort[i_o]=tmp
+    i_o+=1
