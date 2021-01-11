b70076fee697a807db7be487c29ca35d2a1356dc
mbelski99 <mbelski99@gmail.com>
2021-01-11 23:27:04 +0100

    a

---
+++ venv/zadanie3.py
@@ -0,0 +1,12 @@
+word="Słowo"
+x=0
+while x<5:
+    word1=input('Wpisz słowo:')
+    if word1!=word:
+        print ("Źle! Spróbuj jeszcze raz")
+    else:
+        print ("Gratulacje")
+        break
+    x+=1
+if x==5:
+    print ("Koniec prób!")
